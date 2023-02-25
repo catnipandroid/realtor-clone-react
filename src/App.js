80df6b9023a44895f2921432302714f2ca3814f0
박재현 <parkjaehyun@bagjaehyeons-MacBook-Pro.local>
2023-02-25 10:33:18 +0900

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SingUp from './pages/SingUp';
+
 function App() {
   return (
-    <div>
-      <>
-        <h1 className="text-2xl">Hello World</h1>
-      </>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/profile' element={<Profile/>} />
+          <Route path='/sign-in' element={<SignIn/>} />
+          <Route path='/sign-up' element={<SingUp/>} />
+          <Route path='/forgot-password' element={<ForgotPassword/>} />
+          <Route path='/offers' element={<Offers/>} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
