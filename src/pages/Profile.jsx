af5d40db77444e3b698fbeba560e7fe6ed2b462a
박재현 <parkjaehyun@bagjaehyeons-MacBook-Pro.local>
2023-03-12 16:48:47 +0900

    create private route and custom hook for protecting the profile page and log-out functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,61 @@
-import React from 'react'
+import React, { useState } from "react";
+import { getAuth } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 export default function Profile() {
+  const auth = getAuth();
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
+
+  function onLogout() {
+    auth.signOut();
+    navigate("/");
+  }
+
+  const { name, email } = formData;
+
   return (
-    <div>Profile</div>
-  )
+    <>
+      <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
+        <h1 className="text-3xl text-center mt-6 items-center font-bold">
+          My Profile
+        </h1>
+        <div className="w-full md:w-[50%] mt-6 px-3">
+          <form>
+            <input
+              type="text"
+              id="name"
+              className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
+              value={name}
+              disabled
+            />
+            <input
+              type="text"
+              id="email"
+              className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
+              value={email}
+              disabled
+            />
+            <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6">
+              <p className="flex item-center mb-6">
+                Do want change your name?{" "}
+                <span className="text-red-600 hover:text-red-800 transition ease-in-out duration-200 ml-1 cursor-pointer">
+                  Edit
+                </span>
+              </p>
+              <p
+                onClick={onLogout}
+                className="text-blue-600 hover:text-blue-800 cursor-pointer transition duration-200 ease-in-out"
+              >
+                Sign out
+              </p>
+            </div>
+          </form>
+        </div>
+      </section>
+    </>
+  );
 }
